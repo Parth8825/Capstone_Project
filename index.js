e7e97ad5c3aee12de5dbe8b46a9b4f7cde17549c
Parth Darji <pdarji8825@conestogac.on.ca>
2022-03-01 18:43:31 -0500

    convert it to server side
    I have made a major change. From now on it will be on server side

---
+++ index.js
@@ -0,0 +1,50 @@
+//import express
+const { fuchsia } = require('color-name');
+var express =  require('express');
+var path = require('path');
+
+//create an expres
+var ourApp = express();
+
+//set up the path to public folder and views
+ourApp.set('views',path.join(__dirname,'views'));
+
+//set up the path for public stuff like CSS and javascript
+ourApp.use(express.static(__dirname + '/public'));
+
+//define the view engine
+ourApp.set('view engine', 'ejs');
+
+//handle the HTTP requests
+
+
+// Home page (Employee page)
+ourApp.get('/',function(req,res){
+    //res.send('this one was showing in the browser');
+    res.render('employee');
+});
+// Add Employee page
+ourApp.get('/addEmployee',function(req,res){
+    res.render('addEmployee');
+});
+// schedule page
+ourApp.get('/schedule',function(req,res){
+    res.render('schedule');
+});
+// Pay stub page
+ourApp.get('/paystub',function(req,res){
+    res.render('paystub');
+});
+// Inventory page
+ourApp.get('/inventory',function(req,res){
+    res.render('inventory');
+});
+// Attendance page
+ourApp.get('/attendance',function(req,res){
+    res.render('attendance');
+});
+//listen for request at port 8080
+ourApp.listen(8080);
+
+//just printing execution successful
+console.log("execute successfully: localhost:8080");
