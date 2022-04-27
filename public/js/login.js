b6f1880885a8074263f6f6c004210ca1f9115959
Parth Darji <pdarji8825@conestogac.on.ca>
2022-04-27 13:44:44 -0400

    nothing chnaged

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,14 @@
-$("#signup").click(function() {
+$( window ).on( "load", function() {
+var win = $(this);
+if (win.width() >= 600) {
+  $("#signup").click(function() {
     $(".message").css("transform", "translateX(100%)");
     if ($(".message").hasClass("login")) {
       $(".message").removeClass("login");
     }
     $(".message").addClass("signup");
   });
-  
+
   $("#login").click(function() {
     $(".message").css("transform", "translateX(0)");
     if ($(".message").hasClass("login")) {
@@ -13,4 +16,46 @@ $("#signup").click(function() {
     }
     $(".message").addClass("login");
   });
+}
+else if (winTwo.width() <= 600){
+  $("#signup").click(function() {
+    $("html, body").scrollTop($("#sigupPage"). $("body").offset().top);
+  });
+  $("#login").click(function() {
+    $("html, body").scrollTop($("#loginPage"). $("body").offset().top);
+  });
+}
+$( window ).on( "resize", function() {
+  var winTwo = $(this);
+    if (winTwo.width() >= 600) {
+    $("#signup").click(function() {
+      $(".message").css("transform", "translateX(100%)");
+      if ($(".message").hasClass("login")) {
+        $(".message").removeClass("login");
+      }
+      $(".message").addClass("signup");
+    });
+  
+    $("#login").click(function() {
+      $(".message").css("transform", "translateX(0)");
+      if ($(".message").hasClass("login")) {
+        $(".message").removeClass("signup");
+      }
+      $(".message").addClass("login");
+    });
+  }
+  else if (winTwo.width() <= 600){
+    $("#signup").click(function() {
+      $("html, body").scrollTop($("#sigupPage"). $("body").offset().top);
+    });
+    $("#login").click(function() {
+      $("html, body").scrollTop($("#loginPage"). $("body").offset().top);
+    });
+  }
+});
+});
+
+
+
+
   
